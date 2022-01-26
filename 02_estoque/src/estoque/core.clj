ca3fccb9b8072039cad094e0c9d185b24321970f
Gilvan Ritter <gilvan.ritter@nubank.com.br>
2022-01-26 19:31:46 -0300

    Functions inside namespace.
    - Anonymous functions
    - Unnamed anonymous functions with args numbers
    - Higher order functions

---
+++ 02_estoque/src/estoque/core.clj
@@ -0,0 +1,48 @@
+(ns estoque.core)
+
+(defn value-is-eligible?
+      "Returns whether the gross-value is eligible for discount"
+      [gross-value]
+      (> gross-value 100))
+
+(defn is-eligible?
+      "Allows very abstracted stuff to happen"
+      [policy-checker, value]
+      (policy-checker value)
+      )
+
+(fn [gross-value max-value] (> gross-value max-value))
+
+(defn calculate-discount
+      "Returns value with discount if eligible"
+      [is-eligible? gross-value]
+      (if (is-eligible? gross-value)
+        (let [discount-tax (/ 10 100)
+              discount-value (* gross-value discount-tax)]
+             (- gross-value discount-value))
+        gross-value
+        ))
+
+(println (value-is-eligible? 200))
+
+; eg.: anonymous function
+(println (is-eligible?
+           (fn [gross-value] (> gross-value 100))
+           200))
+
+; fn can be replaced with #, together with the arguments declaration
+
+; eg.: anonymous function with unnamed number args %1, %2...
+(println (is-eligible?
+           #(> %1 100)
+           200))
+
+; eg.: anonymous function with single unnamed arg %
+(println (is-eligible?
+           #(> % 100)
+           200))
+
+(println "Value with discount is:"
+         (calculate-discount value-is-eligible? 200))
+
+
